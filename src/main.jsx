df6dbf0a6d20b8fb5164aa3f896a164d8d5f2862
shakhera <shakherakhanom024@gmail.com>
2024-01-14 15:22:54 +0600

    router set and added error page router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Lauout/Root.jsx";
+import Home from "./components/Home/Home.jsx";
+import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "*",
+        element: <ErrorPage></ErrorPage>,
+        errorElement: <ErrorPage></ErrorPage>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
