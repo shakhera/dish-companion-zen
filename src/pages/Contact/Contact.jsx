76e26a0aadc6c384350236d35d712eb03c8a8714
shakhera <shakherakhanom024@gmail.com>
2024-01-30 18:18:19 +0600

    contact us implement

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,50 @@
 import React from "react";
+import contact from "../../assets/contact/contact.png";
 
 const Contact = () => {
   return (
     <div>
-      <h2>contact us</h2>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <div className="md:w-1/2 pr-12 text-center lg:text-left">
+            <img src={contact} alt="" />
+          </div>
+          <div className="card  md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <h1 className="text-5xl font-bold text-center mb-3">
+                Contact Us
+              </h1>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="form-control">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Full name"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-control">
+                <textarea
+                  className="textarea textarea-bordered"
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-info">Button</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
